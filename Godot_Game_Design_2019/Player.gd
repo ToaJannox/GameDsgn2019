b7fd19464747233cb6161bc3e35ef62fcfedd2c7
Zoe Debaty <zdebaty@emi.u-bordeaux1.fr>
2019-11-27 11:27:43 +0100

    recreate Player.gd

---
+++ Godot_Game_Design_2019/Player.gd
@@ -0,0 +1,87 @@
+extends KinematicBody2D
+
+# Declare member variables here.
+var GRAVITY = 1500.0
+
+const FALL_SPEED = 1500
+
+const WALK_SPEED = 300
+const WALK_SPEED_MAX = 300
+
+const LADDER_SPEED = 500
+
+const STOP_FORCE = 3000
+
+const JUMP_SPEED = 500
+const MAX_JUMP_TIME = 0.3
+
+var velocity = Vector2()
+
+var on_ladder = false
+
+var on_air_time = 0
+
+# Called every frame, "delta" is the elapsed time since the previous frame.
+func _process(delta):
+	
+	var motion = Vector2(0, GRAVITY)
+	var stop = true
+	
+	var walk_left = Input.is_action_pressed("ui_left")
+	var walk_right = Input.is_action_pressed("ui_right")
+	var walk_up = Input.is_action_pressed("ui_up")
+	var walk_down = Input.is_action_just_pressed("ui_down")
+	
+	# Movements in x
+	if walk_left && !walk_right:
+		motion.x = -WALK_SPEED
+		$AnimatedSprite.animation = "walk"
+		stop = false
+	elif walk_right && !walk_left:
+		motion.x = WALK_SPEED
+		$AnimatedSprite.animation = "walk"
+		stop = false
+	else:
+		$AnimatedSprite.animation = "static"
+		
+	# On ladder TODO !
+	if on_ladder == true:
+		# Climb
+		if walk_up && !walk_down:
+			motion.y = -LADDER_SPEED
+		# Climb down
+		elif walk_down && !walk_up:
+			motion.y = LADDER_SPEED
+		else:
+			motion.y = 0
+	
+	# Is it flying ?
+	if is_on_floor():
+		on_air_time = 0
+	elif !on_ladder:	
+		on_air_time += delta
+		
+	# Is jumping
+	if walk_up && !on_ladder:
+		if on_air_time < MAX_JUMP_TIME and walk_up:
+			velocity.y = -JUMP_SPEED
+			$AnimatedSprite.animation = "static"
+	
+	# Doesn't move
+	if stop:
+		var vsign = sign(velocity.x)
+		var vlen = abs(velocity.x)
+		
+		vlen -= STOP_FORCE * delta
+		if vlen < 0:
+			vlen = 0
+		
+		velocity.x = (vlen * vsign)
+	
+	# Integrate forces to velocity
+	if(abs(velocity.x) < WALK_SPEED_MAX):
+		velocity.x += motion.x * delta
+	velocity.y += motion.y * delta
+	
+	# Move Player
+	velocity = move_and_slide(velocity, Vector2(0, -1))
