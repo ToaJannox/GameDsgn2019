fb20dc6e2db4bec0f0674703f045fb2eebb91d72
Zoe Debaty <zdebaty@emi.u-bordeaux1.fr>
2019-12-17 19:24:41 +0100

    add dialog and change nodes

--- Godot_Game_Design_2019/Scripts/Door exit 1_1.gd
+++ Godot_Game_Design_2019/Scripts/Door exit 1_1.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 func _on_Exit_1_1_body_entered(body):
 	if body.name == "Player":
-		get_node("../Player").level = 1.2;
+		get_node("../../Player").level = 1.2;
--- Godot_Game_Design_2019/Scripts/Node2D.gd
+++ Godot_Game_Design_2019/Scripts/Node2D.gd
@@ -4,7 +4,7 @@ var level
 var cur_level
 
 func _process(delta):
-	$"Player".stop_1_2 = $"End_slide_1_2".position.x
+	$"Player".stop_1_2 = $"World 1_2"/"End_slide_1_2".position.x
 	
 	level = $"Player".level
 	
@@ -20,7 +20,7 @@ func setVisibilite(canvas):
 		canvas.set_color(Color(1, 1, 1))
 	
 	if cur_level == 1.2:
-		if $Player.position.x > $"End_slide_1_2".position.x + 10 && $Player.is_on_floor():
+		if $Player.position.x > $"World 1_2"/"End_slide_1_2".position.x + 10 && $Player.is_on_floor():
 			$Player.walk_1_2 = true
 			$Player/PlayerSprite.animation = "static"
 			$Player/PlayerSprite.flip_h = true
@@ -36,8 +36,8 @@ func setLevel():
 	var player = $Player
 	
 	if level == 1.1:
-		var x = $"Start 1_1".position.x
-		var y = $"Start 1_1".position.y
+		var x = $"World 1_1"/"Start 1_1".position.x
+		var y = $"World 1_1"/"Start 1_1".position.y
 		player.position.x = x
 		player.position.y = y
 		player.level = 0;
@@ -46,8 +46,8 @@ func setLevel():
 		$Player/PlayerSprite.animation = "static"
 		$Player/PlayerSprite.flip_h = true
 		player.look_right = false
-		var x = $"Start 1_2".position.x
-		var y = $"Start 1_2".position.y
+		var x = $"World 1_2"/"Start 1_2".position.x
+		var y = $"World 1_2"/"Start 1_2".position.y
 		player.position.x = x
 		player.position.y = y
 		player.level = 0;
