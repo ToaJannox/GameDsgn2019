27868d2cb6f2e6f9c1680295b989170780949c5b
robin montferme <robin.montferme@gmail.com>
2020-01-07 19:44:34 +0100

    added ground_type sounds

--- Godot_Game_Design_2019/Scripts/step.gd
+++ Godot_Game_Design_2019/Scripts/step.gd
@@ -1,14 +1,33 @@
 extends AudioStreamPlayer2D
 var old_frame = 0
-var sounds = [
-	preload("res://ressources/Sounds/player/steps/sand/sand1.wav"),
-	preload("res://ressources/Sounds/player/steps/sand/sand2.wav"),
-	preload("res://ressources/Sounds/player/steps/sand/sand3.wav"),
-	preload("res://ressources/Sounds/player/steps/sand/sand4.wav"),
-	preload("res://ressources/Sounds/player/steps/sand/sand5.wav")
-	]
-	
+enum GROUND_TYPE {
+	SAND,
+	STONE
+	}
+var sounds
+
+var currentGroundType = GROUND_TYPE.SAND
+
 func _ready():
+	match currentGroundType:
+		GROUND_TYPE.SAND:
+			sounds =  [
+		preload("res://ressources/Sounds/player/steps/sand/sand1.wav"),
+		preload("res://ressources/Sounds/player/steps/sand/sand2.wav"),
+		preload("res://ressources/Sounds/player/steps/sand/sand3.wav"),
+		preload("res://ressources/Sounds/player/steps/sand/sand4.wav"),
+		preload("res://ressources/Sounds/player/steps/sand/sand5.wav")
+		]
+		GROUND_TYPE.STONE:
+				sounds =  [
+		preload("res://ressources/Sounds/player/steps/stone/stone1.wav"),
+		preload("res://ressources/Sounds/player/steps/stone/stone2.wav"),
+		preload("res://ressources/Sounds/player/steps/stone/stone3.wav"),
+		preload("res://ressources/Sounds/player/steps/stone/stone4.wav"),
+		preload("res://ressources/Sounds/player/steps/stone/stone5.wav"),
+		preload("res://ressources/Sounds/player/steps/stone/stone6.wav"),
+		]
+		
 	randomize()
 	
 		
